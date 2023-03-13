8dd0e4b0bf7f747b2874f146912fed664dfd7775
pbt122049 <phanbatai94@gmail.com>
2023-03-13 17:26:28 +0700

    redesign + restructure + add avatar.jpg for page 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty(
-      "--scroll",
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
-);
-$(".content__slide").slick({
-  infinite: false,
-  adaptiveHeight: true,
-  arrows: true,
-  prevArrow: $(".content__prev"),
-  nextArrow: $(".content__next"),
-})
+
